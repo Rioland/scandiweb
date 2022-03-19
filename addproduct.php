7d41ab5be86bc65ef3d4c3a99d0e9d071695eb13
Adedamola Rioland <riolandadedamola@gmail.com>
2022-03-19 17:23:19 +0100

    add product

--- addproduct.php
+++ addproduct.php
@@ -1,91 +1,112 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Scandiweb</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<link rel="stylesheet" href="css/style.css">
-<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 </head>
+
 <body>
-  
-<form id="product_form">
-<table>
-    <tr>
-        <td>
-           <p>SKU</p> 
-        </td>
-        <td>
-          <input type="text"/> 
-        </td>
-    </tr>
-    <tr>
-        <td>
-           <p>Name</p> 
-        </td>
-        <td>
-           <input type="text"/> 
-        </td>
-    </tr>
-    <tr>
-        <td>
-           <p>Price</p> 
-        </td>
-        <td>
-           <input type="text"/> 
-        </td>
-    </tr>
-    <tr>
-        <td>
-           <p>Product Type</p> 
-        </td>
-        <td>
-           <select id="productType">
-               <option  value="red" id="Dvd">Dvd</option>
-               <option  value="yellow" id="Furniture">Furniture</option>
-               <option  value="blue" id="Book">Book</option>
-           </select>
-        </td>
-    </tr>
+
+    <table>
+        <tr>
+            <td>
+                <p>SKU</p>
+            </td>
+            <td>
+                <input type="text" id="sku" />
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <p>Name</p>
+            </td>
+            <td>
+                <input type="text" id="name" />
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <p>Price</p>
+            </td>
+            <td>
+                <input type="text" id="price" />
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <p>Product Type</p>
+            </td>
+            <td>
+                <select id="productType">
+                    <option value="red" id="Dvd">Dvd</option>
+                    <option value="yellow" id="Furniture">Furniture</option>
+                    <option value="blue" id="Book">Book</option>
+                </select>
+            </td>
+        </tr>
     </table>
 
     <div class="change">
-    <div id="red" class="color"> 
-       <div>
-        <input type="text" placeholder="enter size in mb"/>   
-       </div>    
-    </div>
-    <div id="yellow" class="col">
-    <div>
-        <input type="text" placeholder="enter height in cm"/>   
-       </div>           
-</div>
-    <div id="blue" class="colr">
-    <div>
-        <input type="text" placeholder="enter book number"/>   
-       </div>         
-</div>
+        <div class="color">
+            <div>
+                <input type="text" id="size" placeholder="enter size in mb" />
+            </div>
+        </div>
+
     </div>
 
 
     <div>
-        <button type="submit" class="mt-5 btn btn-primary btn-sm">Submit</button>
-        <button type="submit" class="mt-5 btn btn-danger btn-sm">Cancel</button>
+        <button type="button" id="submit" class="mt-5 btn btn-primary btn-sm">Submit</button>
+        <button type="reset" class="mt-5 btn btn-danger btn-sm">Cancel</button>
     </div>
-</form>
 
 
-<script>
-$(document).ready(function(){
-    $('.col, .colr').hide();
-    $('#productType').change(function(){
-        $('.color ,.col ,.colr').hide();
-        $('#' + $(this).val()).show();
-    });
-  
-});
-</script>
+
+    <script>
+        $(document).ready(function() {
+           
+
+            $("#submit").click(function(e) {
+                e.preventDefault();
+                var name = $("#name").val();
+                var sku = $("#sku").val();
+                var price = $("#price").val();
+                var productType = $("#productType").val();
+                var size = $("#size").val();
+
+                if (name && sku && price && productType && size) {
+                    $.ajax({
+                        type: "post",
+                        url: "api/index.php",
+                        data: {
+                            "product_name":name,
+                            "product_price":price,
+                            "product_size":size,
+                            "sku":sku,
+                            "type":productType
+                        },
+                        dataType: "text",
+                        success: function (response) {
+                            const obj = JSON.parse(response);
+                            alert(obj.message);
+                        }
+                    });
+
+                }else{
+                    alert("All fields are required")
+                }
+
+            });
+
+        });
+    </script>
 </body>
+
 </html>
--- readme.txt
+++ readme.txt
@@ -12,3 +12,6 @@ PUT /api/index.php
 
 // delete an existing record
 DELETE /api/index.php
+
+change db config to your prefer one goto api->inc->config.php
+i am using PDO
